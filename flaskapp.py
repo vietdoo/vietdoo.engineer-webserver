5c02e55cbe447e9a64365c1fd0501eb7359c25e9
vietdoo <dodoviet@icloud.com>
2022-10-18 14:15:30 +0700

    show text

--- flaskapp.py
+++ flaskapp.py
@@ -1,18 +1,7 @@
-
 import os, requests, torch, math, cv2
 import numpy as np
 import PIL
 
-if os.getcwd()=="/content":
-  os.chdir("YOLOv6")
-
-from yolov6.utils.events import LOGGER, load_yaml
-from yolov6.layers.common import DetectBackend
-from yolov6.data.data_augment import letterbox
-from yolov6.utils.nms import non_max_suppression
-from yolov6.core.inferer import Inferer
-
-from typing import List, Optional
 
 from flask import Flask,jsonify,request,render_template,send_from_directory
 from source.utils import draw_rectangles, read_image, prepare_image
@@ -41,10 +30,12 @@ def home():
 def upload():
     file = request.files['image']
     image = read_image(file)
-    image = run_model(image)
+    
+    image, text = run_model(image)
     to_send = prepare_image(image)
     faces = [1, 2]
-    return render_template('index.html', face_detected=len(faces)>0, num_faces=len(faces), image_to_show=to_send, init=True)
+    #print(len(text)>0)
+    return render_template('index.html', face_detected=len(text)>0, num_faces=text, image_to_show=to_send, init=True)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0')
