c4401237864195b9cbcd0a0b2dbc297085ad5ac6
vietdoo <dodoviet@icloud.com>
2023-01-10 20:59:27 +0700

    maps contrib

--- static/maps/js/main.js
+++ static/maps/js/main.js
@@ -42,12 +42,12 @@ $(function() {
 
 // slider-end
 
-var tiles = L.tileLayer('https://{s}.tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
-	attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+var tiles = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
 	minZoom: 0,
-	maxZoom: 22,
-	subdomains: 'vietdoo.engineer',
-	accessToken: 'pJpka0tsqIgzXVmA0r4SfUN4XjO8SmNVLFaytim0xB6eX5sx0yzE476KZbqlWXiK'
+	maxZoom: 18,
+	ext: 'png'
 }),
     latlng = L.latLng(10.773081, 106.6829); 
 
