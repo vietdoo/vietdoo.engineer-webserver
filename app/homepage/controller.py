c71a9f3a7f616ee89bf6827670b831be38b723aa
Đỗ Quốc Việt <20280115@student.hcmus.edu.vn>
2023-02-06 23:15:48 +0700

    subdomain

--- app/homepage/controller.py
+++ app/homepage/controller.py
@@ -6,3 +6,8 @@ home_page = Blueprint('home', __name__, url_prefix='/')
 @home_page.route('/')
 def home():
   return render_template('homepage/index.html')
+
+
+@home_page.route('/', subdomain ='practice')
+def practice():
+    return "Coding Practice Page"
